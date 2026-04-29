7e2d7db9028260ca6e6513898db4cde47f9bfa2d
carloszetti10 <carloszetti07@gmail.com>
2026-04-29 11:01:29 -0300

    item venda

---
+++ Source/Model/uItemVendaModel.pas
@@ -0,0 +1,45 @@
+unit uVendaModel;
+
+interface
+type
+   TStatusVenda = (svAberta = 0, svDav = 1, svFaturada = 2, svCancelado = 3);
+   TVendaModel = class
+    private
+      FIdVenda: Integer;
+      FIdVendedor: Integer;
+      FIdCliente: Integer;
+      FIdUsuario: Integer;
+
+      FNomeVendedor: string;
+      FNomeCliente: string;
+      
+      FStatus: TStatusVenda;
+      FValorTotal: Currency;
+      FValorDesc: Currency;
+      FEmissao: TDateTime;
+    public
+      constructor Create;
+      property IdVenda: Integer read FIdVenda write FIdVenda;
+      property IdVendedor: Integer read FIdVendedor write FIdVendedor;
+      property IdCliente: Integer read FIdCliente write FIdCliente;
+      property IdUsuario: Integer read FIdUsuario write FIdUsuario;
+
+      property NomeVendedor: string read FNomeVendedor write FNomeVendedor;
+      property NomeCliente: string read FNomeCliente write FNomeCliente;
+
+      property Status: TStatusVenda read FStatus write FStatus;
+      property ValorTotal: Currency read FValorTotal write FValorTotal;
+      property ValorDesconto: Currency read FValorDesc write FValorDesc;
+      property Emissao: TDateTime read FEmissao write FEmissao;
+  end;
+
+implementation
+
+{ TVendaModel }
+
+constructor TVendaModel.Create;
+begin
+  FStatus := svAberta;
+end;
+
+end.
